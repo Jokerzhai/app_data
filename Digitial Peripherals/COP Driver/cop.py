09ed71598578f35bfe485602249edf55550bc9b7
Joker Zhai(nxf45038) <zhiyu.zhai@nxp.com>
2019-07-05 17:03:11 +0800

    update cop.py and gint.py

--- Digitial Peripherals/COP Driver/cop.py
+++ Digitial Peripherals/COP Driver/cop.py
@@ -2,11 +2,8 @@ from .driver_examples import *
 class Case(driver_examples):
     def __init__(self):
         super(Case,self).__init__()
-        self.testExpectedPatterns = ['COP\s*example\s*start!',
-                                    'COP\srefresh\s10\stime',
-                                    'COP\swill\stimeout\sand\schip\swill\sbe\sreset',
-                                    'Reset\sdue\sto\sCOP\stimeout', 
-                                    'COP\sexample\sends!']
-
-    def interactive(self):
-        self.test(expectedPatterns = self.testExpectedPatterns)
+        self.expectedPatterns = ["COP example start!",
+                                    "COP refresh 10 time",
+                                    "COP will timeout and chip will be reset",
+                                    "Reset due to COP timeout", 
+                                    "COP example ends!"]
--- Digitial Peripherals/GINT Driver/gint.py
+++ Digitial Peripherals/GINT Driver/gint.py
@@ -1,31 +1,21 @@
 from .driver_examples import *
-class ValidateBase(object):
-        self.CPLD_CMD = CPLD_CMD
-    def cpld_cmd(self):
-        if self.CPLD_CMD:
-            self.case.assistSpawn.write(self.CPLD_CMD + "\r\n")
-            time.sleep(1)
 
 class Case(driver_examples):
     def __init__(self):
-        super(Case,self).__init__()
-        self.CPLD_CMD = "$RST"
+        super(Case, self).__init__()
+        self.CPLD_RST = "$RST"
         self.CPLD_LED = "$GIN"
-
-        self.testExpectedPatterns = ['GINT0 event detected',
-                                    'GINT1 event detected']
-        
-    #    self.CPLDPressingButton = None
-    
+        self.CPLDPressingButton = "PRESS_BUTTON" 
+        self.testExpectedPatterns = ["GINT0 event detected",
+                                    "GINT1 event detected"]
     def interactive(self):
-        self.test(expectedPatterns = self.testExpectedPatterns)
         self.assistSpawn.write('sreset --hard\r\n')
         self.assistSpawn.write(self.CPLD_RST + "\r\n")
+        time.sleep(1)
         self.assistSpawn.write(self.CPLD_LED + "\r\n")
         time.sleep(4)
-        for num in range(1,2):
+        for num in range(1, 2):
             self.assistSpawn.write(self.CPLDPressingButton + "\r\n")
             time.sleep(2)
-        
+        self.test(expectedPatterns = self.testExpectedPatterns)
         self.assistSpawn.write(self.CPLD_RST + "\r\n")
-        
